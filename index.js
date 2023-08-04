c60506c375e60e4bd549b456e44932a013364503
Tarik Uyan <musician.tarik@gmx.de>
2023-08-04 19:27:14 +0200

    pls

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const http = require('http').createServer();
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
