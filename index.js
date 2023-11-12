25fea1274a2529cca8f6cf3a82f524c46d42081c
Tarik Uyan <musician.tarik@gmx.de>
2023-11-12 21:05:20 +0100

    transporter

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ const {CLIENT_ID, CLIENT_SECRET, USER, REFRESH_TOKEN} = process.env;
 
 // oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
-
+let transporter;
 async function mailSetup() {
     const testUser = await createTestAccount();
     const {user, pass} = testUser;
     console.log(`user: ${user}, pass: ${pass}`)
     // const ACCESS_TOKEN = await oAuth2Client.getAccessToken();
-    const transporter = createTransport({
+    transporter = createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
         secure: false,
