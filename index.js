9946a665ea85cea96c461db21bd69525e694677a
Tarik Uyan <musician.tarik@gmx.de>
2023-11-13 09:25:55 +0100

    asdasdasd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {createServer} from 'http';
 import {createTransport, createTestAccount, getTestMessageUrl} from 'nodemailer';
 import {Server} from 'socket.io';
-import {google} from 'googleapis';
+// import {google} from 'googleapis';
 const http = createServer();
 
 const port = process.env.PORT;
@@ -13,40 +13,48 @@ const io = new Server(http, {
 
 let users = {};
 
-const {CLIENT_ID, CLIENT_SECRET, USER, REFRESH_TOKEN, PASS} = process.env;
+// const {CLIENT_ID, CLIENT_SECRET, USER, REFRESH_TOKEN, PASS} = process.env;
+const CLIENT_ID = "470427326628-ld6u3mi3eh604ipc3g9bb7pj1h69mfku.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const USER = "noreply.info.join@gmail.com"
+const REFRESH_TOKEN = "1//047DtEF64hTClCgYIARAAGAQSNwF-L9IrBX9XDIfjFmHOtmqX0BvcXKkW5qrjGya7fXgiOXbSm2WjJAJeLnN0brCrBNDnsigHJYo"
+const ACCESS_TOKEN = "ya29.a0AfB_byBPuoQIMNuBQclQXfLizhie5EADJ6E8v34_TWq76xf-bgA5kffVxpFBxcy6kvTkzmA7JWITvlB0C-Nfcul7WQFUHJbS3CVAO58R95lyBKmzGgYnHJu5p4Zms-uOEU_kiaK4iJWI9y4GeTAsKmIAMRB93uJuHU2aaCgYKAcQSARESFQHGX2MimpSEkjrdbAF7HOTMcJ8pPQ0171";
+
 console.log(`user: ${USER}`);
 console.log(`client_id: ${CLIENT_ID}`);
 console.log(`client_secret: ${CLIENT_SECRET}`);
 console.log(`refresh_token: ${REFRESH_TOKEN}`);
+console.log(`access: ${ACCESS_TOKEN}`);
 
 const REDIRECT_UI = "https://developers.google.com/oauthplayground";
 
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_UI
-)
+// const oAuth2Client = new google.auth.OAuth2(
+//     CLIENT_ID,
+//     CLIENT_SECRET,
+//     REDIRECT_UI
+// )
 
-oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+// oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
 let transporter;
 async function mailSetup() {
     // const testUser = await createTestAccount();
     // const {user, pass} = testUser;
-    const ACCESS_TOKEN = await new Promise((resolve, reject) => {
-        oAuth2Client.getAccessToken((error, token) => {
-            if (error) reject(error);
-            resolve(token);
-        })
-    })
-    console.log(`access_token: ${ACCESS_TOKEN}`)
+    // const ACCESS_TOKEN = await new Promise((resolve, reject) => {
+    //     oAuth2Client.getAccessToken((error, token) => {
+    //         if (error) reject(error);
+    //         resolve(token);
+    //     })
+    // })
+    // console.log(`access_token: ${ACCESS_TOKEN}`)
     transporter = createTransport({
+        service: "gmail",
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
             type: "OAuth2",
-            USER,
+            user: USER,
             // PASS
             clientId: CLIENT_ID,
             clientSecret: CLIENT_SECRET,
@@ -63,15 +71,16 @@ mailSetup();
 async function sendEmail({to, subject, html}) {
     
     const mailOptions = {
-        from: `Join <${USER}>`,
+        from: USER,
         to,
         subject,
         html
     }
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) return console.log(error);
-        const preview = getTestMessageUrl(info);
-        console.log(preview)
+        console.log("Message sent!")
+        // const preview = getTestMessageUrl(info);
+        // console.log(preview)
     });
     
 }
