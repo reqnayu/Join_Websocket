a7b2fc1b2f26d32e7b3f17e3368cc9a94e4e92b3
Tarik Uyan <musician.tarik@gmx.de>
2023-11-14 12:32:22 +0100

    list

--- upload.js
+++ upload.js
@@ -1,39 +1,46 @@
-import { google } from 'googleapis';
-import { Duplex } from 'stream';
-import fs from 'fs';
+import { google } from "googleapis";
+import { Duplex } from "stream";
 
 function getDrive() {
-    const keyFile = 'apiKey.json';
-    const scopes = ['https://www.googleapis.com/auth/drive'];
+    const keyFile = "apiKey.json";
+    const scopes = ["https://www.googleapis.com/auth/drive"];
 
     const auth = new google.auth.GoogleAuth({ keyFile, scopes });
 
-    const driveService = google.drive({ version: 'v3', auth });
+    const driveService = google.drive({ version: "v3", auth });
     return driveService;
 }
 
 const drive = getDrive();
 
 async function uploadImg(file, fileName) {
-    const folderId = "1yEznhW0rMVCmOO5oNeKCHRLz9TkFjFcp";
-    const readableStream = new Duplex();
-    readableStream.push(file);
-    readableStream.push(null);
+  const folderId = "1yEznhW0rMVCmOO5oNeKCHRLz9TkFjFcp";
 
-    // const readableStream = fs.createReadStream("1689153888103_5954.jpg");
-    // drive.files.delete
-    const { data: { id, name }} = await drive.files.create({
-        resource: {
-          name: fileName,
-          parents: [folderId],
-        },
-        media: {
-          mimeType: `image/*`,
-          body: readableStream,
-        },
-        fields: 'id,name',
-      });
-      console.log(`File uploaded: ${id}, ${name}`);
+  const list = await drive.files.list({
+    driveId: folderId
+  });
+  console.dir(`files`, list);
+  return;
+  await uploadFile(folderId, file, fileName);
+}
+
+async function uploadFile(folderId, file, fileName) {
+  const readableStream = new Duplex();
+  readableStream.push(file);
+  readableStream.push(null);
+
+  const {data: { id, name }} = await drive.files.create({
+    resource: {
+      name: fileName,
+      parents: [folderId],
+    },
+    media: {
+      mimeType: `image/*`,
+      body: readableStream,
+    },
+    fields: "id,name",
+  });
+  console.log(`File uploaded: ${id}, ${name}`);
 }
 
 export { uploadImg };
